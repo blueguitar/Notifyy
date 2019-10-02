4ae90f707d332d7e0352f7860ef4befad8983e1f
Suada Haji <suada.haji@andela.com>
2019-10-02 16:03:04 +0300

    chore: Replace app icon

--- app/src/main/java/com/suadahaji/notify/MainActivity.kt
+++ app/src/main/java/com/suadahaji/notify/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.suadahaji.notify
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
         setContentView(R.layout.activity_main)
     }
 }
