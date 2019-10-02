d9033eca9d6fe8a21f6df6f2bf93e29903246c6f
Suada Haji <suada.haji@andela.com>
2019-10-03 00:09:39 +0300

    chore: Navigate to Edit Note Fragment

--- app/src/main/java/com/suadahaji/notify/MainActivity.kt
+++ app/src/main/java/com/suadahaji/notify/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.suadahaji.notify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
