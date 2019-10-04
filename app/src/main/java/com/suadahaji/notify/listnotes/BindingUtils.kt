e48102dfc4faad34019ed942803e56456651dfe5
Suada Haji <suada.haji@andela.com>
2019-10-04 16:13:08 +0300

    chore: Add NotesListAdapter

---
+++ app/src/main/java/com/suadahaji/notify/listnotes/BindingUtils.kt
@@ -0,0 +1,47 @@
+package com.suadahaji.notify.listnotes
+
+import android.annotation.SuppressLint
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.suadahaji.notify.database.Note
+import java.text.SimpleDateFormat
+
+@BindingAdapter("noteTitle")
+fun TextView.setNoteTitle(item: Note?) {
+    item?.let {
+        text = item.noteTitle
+    }
+}
+
+@BindingAdapter("noteContent")
+fun TextView.setNoteContent(item: Note?) {
+    item?.let {
+        text = item.noteContent
+    }
+}
+
+@BindingAdapter("noteDate")
+fun TextView.setNoteDate(item: Note?) {
+    item?.let {
+        text = convertLongToDateString(item.updatedDate)
+    }
+}
+
+@BindingAdapter("noteTime")
+fun TextView.setNoteTime(item: Note?) {
+    item?.let {
+        text = convertLongToDateString(item.updatedDate)
+    }
+}
+
+@SuppressLint("SimpleDateFormat")
+fun convertLongToDateString(systemTime: Long): String {
+    return SimpleDateFormat("dd-MMM-yyyy")
+        .format(systemTime).toString()
+}
+
+@SuppressLint("SimpleTimeFormat")
+fun convertLongToTimeString(systemTime: Long): String {
+    return SimpleDateFormat("HH:mm")
+        .format(systemTime).toString()
+}
